f37670749a7101ef3ba77e86cdd53f94b1b389de
Lane Gebbie <166470962+lanedg@users.noreply.github.com>
2025-02-27 20:53:54 -0700

    Add random computer choice function

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
+function getComputerChoice() {
+  let randChoice = Math.floor(Math.random() * 3) + 1;
+  if (randChoice === 1) {
+    return "rock";
+  } else if (randChoice === 2) {
+    return "paper";
+  } else if (randChoice === 3) {
+    return "scissors";
+  } else {
+    return "error";
+  }
+}
 
+function getHumanChoice() {}
