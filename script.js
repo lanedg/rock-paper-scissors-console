637e45a627bcd944692beab041cf33e7d613f821
Lane Gebbie <166470962+lanedg@users.noreply.github.com>
2025-02-28 19:29:17 -0700

    Add player round and play game function

--- script.js
+++ script.js
@@ -12,18 +12,60 @@ function getComputerChoice() {
 }
 
 function getHumanChoice() {
-  let humanChoice = prompt("Rock, Paper, or Scissors: ");
-  return humanChoice.toLowerCase();
+  let humanChoice;
+  while (
+    humanChoice !== "rock" &&
+    humanChoice !== "paper" &&
+    humanChoice !== "scissors"
+  ) {
+    humanChoice = prompt("Rock, Paper, or Scissors: ");
+    humanChoice = humanChoice.toLowerCase();
+  }
+  return humanChoice;
 }
 
-function playRound(humanChoice, computerChoice) {
-  console.log(humanChoice);
-  console.log(computerChoice);
+function playRound() {
+  humanChoice = getHumanChoice();
+  computerChoice = getComputerChoice();
+  console.log(`Player choice: ${humanChoice}
+Computer choice: ${computerChoice}`);
+  if (humanChoice === computerChoice) {
+    console.log(`This rounds a tie.`);
+  } else if (humanChoice === "rock" && computerChoice === "paper") {
+    console.log("You lose this round. Paper beats rock.");
+    ++computerScore;
+  } else if (humanChoice === "paper" && computerChoice === "scissors") {
+    console.log("You lose this round. Scissors beats paper.");
+    ++computerScore;
+  } else if (humanChoice === "scissors" && computerChoice === "rock") {
+    console.log("You lose this round. Rock beats scissors.");
+    ++computerScore;
+  } else {
+    console.log(
+      `You win this round. ${
+        humanChoice.charAt(0).toUpperCase() + humanChoice.slice(1)
+      } beats ${computerChoice}.`
+    );
+    ++humanScore;
+  }
+  console.log("\n\n");
 }
 
-const humanSelection = getHumanChoice();
-const computerSelection = getComputerChoice();
+function playGame(playRound) {
+  playRound();
+  playRound();
+  playRound();
+  playRound();
+  playRound();
+  if (humanScore === computerScore) {
+    console.log("Game over.\nIt's a draw......");
+  } else if (humanScore > computerScore) {
+    console.log("Game over.\nYour the winner!");
+  } else {
+    console.log("Game over.\nYou lose......");
+  }
+}
+
 let humanScore = 0;
 let computerScore = 0;
-
-playRound(humanSelection, computerSelection);
+playGame(playRound);
