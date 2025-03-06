f63c671d1f46b7ba3783b811bbf983b91ef2a1ca
Lane Gebbie <166470962+lanedg@users.noreply.github.com>
2025-03-05 23:02:14 -0700

    Add play round functionality

--- script.js
+++ script.js
@@ -24,31 +24,33 @@ function getHumanChoice() {
   return humanChoice;
 }
 
-function playRound() {
-  humanChoice = getHumanChoice();
+function playRound(humanChoice) {
   computerChoice = getComputerChoice();
   console.log(`Player choice: ${humanChoice}
 Computer choice: ${computerChoice}`);
   if (humanChoice === computerChoice) {
-    console.log(`This rounds a tie.`);
+    roundText.textContent = "This rounds a tie.";
   } else if (humanChoice === "rock" && computerChoice === "paper") {
-    console.log("You lose this round. Paper beats rock.");
+    roundText.textContent = "You lose this round. Paper beats rock.";
     ++computerScore;
   } else if (humanChoice === "paper" && computerChoice === "scissors") {
-    console.log("You lose this round. Scissors beats paper.");
+    roundText.textContent = "You lose this round. Scissors beats paper.";
     ++computerScore;
   } else if (humanChoice === "scissors" && computerChoice === "rock") {
-    console.log("You lose this round. Rock beats scissors.");
+    roundText.textContent = "You lose this round. Rock beats scissors.";
     ++computerScore;
   } else {
-    console.log(
-      `You win this round. ${
-        humanChoice.charAt(0).toUpperCase() + humanChoice.slice(1)
-      } beats ${computerChoice}.`
-    );
+    roundText.textContent = `You win this round. ${
+      humanChoice.charAt(0).toUpperCase() + humanChoice.slice(1)
+    } beats ${computerChoice}.`;
     ++humanScore;
   }
-  console.log("\n\n");
+  updateCount();
+}
+
+function updateCount() {
+  humanCounter.textContent = `Human Score : ${humanScore}`;
+  computerCounter.textContent = `Computer Score : ${computerScore}`;
 }
 
 function playGame(playRound) {
@@ -68,3 +70,22 @@ function playGame(playRound) {
 
 let humanScore = 0;
 let computerScore = 0;
+const roundText = document.querySelector("#round-state");
+const rpsContainer = document.querySelector("#rps-container");
+const humanCounter = document.querySelector("#human-score");
+const computerCounter = document.querySelector("#computer-score");
+
+rpsContainer.addEventListener("click", function (event) {
+  let target = event.target;
+  switch (target.id) {
+    case "rock":
+      playRound("rock");
+      break;
+    case "paper":
+      playRound("paper");
+      break;
+    case "scissors":
+      playRound("scissors");
+      break;
+  }
+});
